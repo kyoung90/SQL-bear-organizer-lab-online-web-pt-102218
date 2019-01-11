eb71afccd4d3f72d8038525dba5ae76f1b52ce44
Kenneth Young Castro <kenneth.young90@gmail.com>
2019-01-11 15:40:33 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -2,11 +2,19 @@ INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES (
   "Mr. Chocolate", 5, "M", "Brown", "aggressive", 0
 );
 
-Mr. Chocolate
-Rowdy
-Tabitha
-Sergeant Brown
-Melissa
-Grinch
-Wendy
-unnamed (the bear that killed Tim didn't have a name; refer back to how to create a record that doesn't have one value)
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES (
+  "Rowdy", 3, "M", "Brown", "friendly", 0
+);
+
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES (
+  "Tabitha", 10, "F", "White", "friendly", 1
+);
+
+-- Mr. Chocolate
+-- Rowdy
+-- Tabitha
+-- Sergeant Brown
+-- Melissa
+-- Grinch
+-- Wendy
+-- unnamed (the bear that killed Tim didn't have a name; refer back to how to create a record that doesn't have one value)
