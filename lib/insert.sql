89dd8496bdbcf5f6b78756aaceadad3c9b766502
Kenneth Young Castro <kenneth.young90@gmail.com>
2019-01-11 15:37:50 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,5 +1,5 @@
 INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES (
-  "Mr. Chocolate", 5, "M", "Brown", "aggressive", 
+  "Mr. Chocolate", 5, "M", "Brown", "aggressive",
 );
 
 Mr. Chocolate
